41062fd287e799fcb2a89b1c9c4e8d3896cdcdd1
Mariusz Śliwa <ms204@wp.pl>
2018-05-31 21:02:27 +0200

    Service Types added

--- BamboAuto/Controllers/ServiceTypesController.cs
+++ BamboAuto/Controllers/ServiceTypesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BamboAuto.Data;
 using BamboAuto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BamboAuto.Controllers
 {
@@ -30,6 +31,8 @@ namespace BamboAuto.Controllers
         }
 
         //POST: Services/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ServiceType serviceType)
         {
             if (ModelState.IsValid)
@@ -39,6 +42,77 @@ namespace BamboAuto.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(serviceType);
+        }
+
+        //Details: ServiceTypes/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        //Edit: ServiceTypes/Edit/1
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ServiceType serviceType)
+        {
+            if (id != serviceType.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(serviceType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+            //Delete: ServiceTypes/Delete/1
+            public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveServiceType(int id)
+        {
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            _db.ServiceTypes.Remove(serviceType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         protected override void Dispose(bool disposing)
